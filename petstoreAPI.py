bd86aff3f88fb4114544d7eec0afffdb85c8f376
Vinay <vinayhg87@gmail.com>
2019-11-05 17:24:20 +0530

    Added if else condition

--- petstoreAPI.py
+++ petstoreAPI.py
@@ -3,13 +3,15 @@ import requests as request
 class bigdata:
     def apicall(self):
         response = request.get("https://petstore.swagger.io/v2/pet/findByStatus?status=available")
-        print(response.status_code)
-        jsondata = response.json()
-        print(jsondata)
-        for listdata in jsondata:
-            for key, value in listdata.items():
-                if key == "category":
-                    print(value)
+        if response.status_code == 200:
+            jsondata = response.json()
+            print(jsondata)
+            for listdata in jsondata:
+                for key, value in listdata.items():
+                    if key == "category":
+                        print(value)
+        else:
+            print("Call Failed with response code "+str(response.status_code))
 
 
 
